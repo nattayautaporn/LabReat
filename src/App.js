22cb87bf51886ace50a45fb45d8c973097ecf8e9
nattayautaporn <parn_6740@hotmail.com>
2018-09-13 10:36:53 +0700

    HelloWord1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
 class App extends Component {
-  render() {
-    return (
-      <div>
-    <CharacterCard value="h"/>
-    <CharacterCard value="i"/>
+ render() {
+ return (
+ <div className="App">
+ Hello World
  </div>
-
-    );
-  }
+ );
+ }
 }
-
 export default App;
