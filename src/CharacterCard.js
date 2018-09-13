1621a351a637c1c0e006e290a96f5dbb39e8ecdb
nattayautaporn <parn_6740@hotmail.com>
2018-09-13 11:57:20 +0700

    Handler as Props

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,28 +1,31 @@
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-    export default class CharacterCard extends Component {
-        constructor(props){
+export default class CharacterCard extends Component {
+    constructor(props) {
         super(props)
         this.state = {
-        active: false,
+            active: false,
         }
+    }
+    activate = () => {
+        if (!this.state.active) {
+            this.props.activationHandler(this.props.value)
+            this.setState({ active: true })
         }
-       
-        activate = () => {
-        if(!this.state.active){
-        this.setState({active: true})
+    }
+    activate = () => {
+        if (!this.state.active) {
+            this.setState({ active: true })
         }
-        }
-        render(){
-        let className = `card ${this.state.active ? 'activeCard': ''}`
+    }
+    render() {
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
-        <div className={className} onClick={this.activate}>
-        {this.props.value}
-        </div>
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
         )
-        }
-       }
+    }
+}
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,14 +1,27 @@
 import CharacterCard from './CharacterCard'
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
-export default class WordCard extends
-Component {
-render() {
- return (
- <div>
-{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
- </div>
- );
- }
-}
+export default class WordCard extends Component{
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            attempt: '',
+        }
+        
+    }
+    activationHandler = c => { console.log(`${c} has been activated.`) }
+ 
+            render() {
+                return (
+                    <div>
+                        {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                            activationHandler={this.activationHandler} />
+                        )}
+                    </div>
+                );
+            }
+        }
+    
