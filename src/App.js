c666b675ae458bb7873e5105aa471d627ae2a66d
nattayautaporn <parn_6740@hotmail.com>
2018-09-13 10:43:40 +0700

    edit app Hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello";
+
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
+  render() {
+    return (
+      <div>{
+    Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)>
+      }
  </div>
- );
- }
+
+    );
+  }
 }
+
 export default App;
